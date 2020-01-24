e3d9072ae0541ee86d946c3d385a34cdb3d0751d
zuoyanjide <zuoyanjide@163.com>
2020-01-24 22:41:43 +0800

    Add files via upload

---
+++ QT5/mainwindow.cpp
@@ -0,0 +1,212 @@
+#include <QtWidgets>
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+static Gamepad_Thread *gamepad_thread;
+
+const QString BTN_color("background-color: rgb(255, 32, 85);");
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    gamepad_thread = new Gamepad_Thread();
+    qRegisterMetaType<joyinfoex_tag>("joyinfoex_tag");//注册一种信号的参数类型
+    connect(gamepad_thread, SIGNAL(JoySignal_row(joyinfoex_tag)), this, SLOT(display_slot_row(joyinfoex_tag)));
+    gamepad_thread->start();
+
+
+
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::clearDisplay()
+{
+    ui->PB_A->setStyleSheet("");
+    ui->PB_B->setStyleSheet("");
+    ui->PB_X->setStyleSheet("");
+    ui->PB_Y->setStyleSheet("");
+
+    ui->PB_LB->setStyleSheet("");
+    ui->PB_LT->setStyleSheet("");
+    ui->PB_RB->setStyleSheet("");
+    ui->PB_RT->setStyleSheet("");
+
+    ui->POV_F->setStyleSheet("");
+    ui->POV_B->setStyleSheet("");
+    ui->POV_L->setStyleSheet("");
+    ui->POV_R->setStyleSheet("");
+
+    ui->PB_BACK->setStyleSheet("");
+    ui->PB_START->setStyleSheet("");
+
+    ui->SK_LU->setStyleSheet("");
+    ui->SK_LD->setStyleSheet("");
+    ui->SK_LR->setStyleSheet("");
+    ui->SK_LL->setStyleSheet("");
+    ui->PB_LP->setStyleSheet("");
+
+    ui->SK_RU->setStyleSheet("");
+    ui->SK_RD->setStyleSheet("");
+    ui->SK_RL->setStyleSheet("");
+    ui->SK_RR->setStyleSheet("");
+    ui->PB_RP->setStyleSheet("");
+}
+
+void MainWindow::display_slot_row(joyinfoex_tag state_row){
+    QString s;
+    s.append("dwFlags=").append(QString::number(state_row.dwFlags)).append("\n");
+    s.append("dwXpos=").append(QString::number(state_row.dwXpos)).append("\n");
+    s.append("dwYpos=").append(QString::number(state_row.dwYpos)).append("\n");
+    s.append("dwZpos=").append(QString::number(state_row.dwZpos)).append("\n");
+    s.append("dwRpos=").append(QString::number(state_row.dwRpos)).append("\n");
+    s.append("dwButtons=").append(QString::number(state_row.dwButtons)).append("\n");
+    s.append("dwButtonNumber=").append(QString::number(state_row.dwButtonNumber)).append("\n");
+
+
+    ui->textEdit->setText(s);
+
+    clearDisplay();
+
+    QString select = ui->comboBox->currentText();
+
+    if(0 == select.compare("北通龙腾")) {
+        if(state_row.dwXpos > 65000) {
+            ui->SK_LR->setStyleSheet(BTN_color);
+
+        } else if(state_row.dwXpos < 500) {
+            ui->SK_LL->setStyleSheet(BTN_color);
+        }
+
+        if(state_row.dwYpos > 65000) {
+            ui->SK_LD->setStyleSheet(BTN_color);
+
+        } else if(state_row.dwYpos < 500) {
+            ui->SK_LU->setStyleSheet(BTN_color);
+        }
+
+        if(state_row.dwRpos > 65000) {
+            ui->SK_RD->setStyleSheet(BTN_color);
+
+        } else if(state_row.dwRpos < 500) {
+            ui->SK_RU->setStyleSheet(BTN_color);
+        }
+
+        if(state_row.dwUpos > 65000) {
+            ui->SK_RR->setStyleSheet(BTN_color);
+
+        } else if(state_row.dwUpos < 500) {
+            ui->SK_RL->setStyleSheet(BTN_color);
+        }
+
+        if(state_row.dwZpos > 65000) {
+            ui->PB_LT->setStyleSheet(BTN_color);
+
+        } else if(state_row.dwZpos < 500) {
+            ui->PB_RT->setStyleSheet(BTN_color);
+        }
+
+        if(state_row.dwButtons & 0x01 << 0) {
+            ui->PB_A->setStyleSheet(BTN_color);
+        }
+
+        if(state_row.dwButtons & 0x01 << 1) {
+            ui->PB_B->setStyleSheet(BTN_color);
+        }
+
+        if(state_row.dwButtons & 0x01 << 2) {
+            ui->PB_X->setStyleSheet(BTN_color);
+        }
+
+        if(state_row.dwButtons & 0x01 << 3) {
+            ui->PB_Y->setStyleSheet(BTN_color);
+        }
+
+        if(state_row.dwButtons & 0x01 << 4) {
+            ui->PB_LB->setStyleSheet(BTN_color);
+
+        }
+
+        if(state_row.dwButtons & 0x01 << 5) {
+            ui->PB_RB->setStyleSheet(BTN_color);
+        }
+
+        if(state_row.dwButtons & 0x01 << 6) {
+            ui->PB_BACK->setStyleSheet(BTN_color);
+
+        }
+
+        if(state_row.dwButtons & 0x01 << 7) {
+            ui->PB_START->setStyleSheet(BTN_color);
+        }
+
+        if(state_row.dwButtons & 0x01 << 8) {
+            ui->PB_LP->setStyleSheet(BTN_color);
+
+        }
+
+        if(state_row.dwButtons & 0x01 << 9) {
+            ui->PB_RP->setStyleSheet(BTN_color);
+        }
+
+        if(state_row.dwPOV == 0) {
+            ui->POV_F->setStyleSheet(BTN_color);
+
+        } else if(state_row.dwPOV == 9000) {
+            ui->POV_R->setStyleSheet(BTN_color);
+
+        } else if(state_row.dwPOV == 18000) {
+            ui->POV_B->setStyleSheet(BTN_color);
+
+        } else if(state_row.dwPOV == 27000) {
+            ui->POV_L->setStyleSheet(BTN_color);
+
+        } else if(state_row.dwPOV == 4500) {
+            ui->POV_F->setStyleSheet(BTN_color);
+            ui->POV_R->setStyleSheet(BTN_color);
+
+        } else if(state_row.dwPOV == 31500) {
+            ui->POV_F->setStyleSheet(BTN_color);
+            ui->POV_L->setStyleSheet(BTN_color);
+
+        } else if(state_row.dwPOV == 13500) {
+            ui->POV_R->setStyleSheet(BTN_color);
+            ui->POV_B->setStyleSheet(BTN_color);
+
+        } else if(state_row.dwPOV == 22500) {
+            ui->POV_L->setStyleSheet(BTN_color);
+            ui->POV_B->setStyleSheet(BTN_color);
+        }
+
+    } else if(0 == select.compare("北通_无线")) {
+
+    }
+}
+
+void MainWindow::on_MainWindow_tabifiedDockWidgetActivated(QDockWidget *dockWidget)
+{
+
+}
+
+
+
+
+
+void MainWindow::about()
+//! [13] //! [14]
+{
+   QMessageBox::about(this, tr("About Application"),
+            tr("The <b>Application</b> example demonstrates how to "
+               "write modern GUI applications using Qt, with a menu bar, "
+               "toolbars, and a status bar."));
+}
+
+
+
+
